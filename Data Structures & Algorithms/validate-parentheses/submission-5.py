65a752df1a864c3836c19361dc2e113e00c7c5ba
Rohan Chopra <100032849+rchops@users.noreply.github.com>
2026-08-11 15:32:22 +0100

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # use hashmap with closing for keys for each pair
+        # use stack for comparisons
+        # if its close parenthesis check if val = top of stack
+        # otherwise means its still opening and add to stack
+        close_to_open = {")":"(", "}":"{", "]":"["}
+        stack = []
+        for c in s:
+            if c in close_to_open:
+                if stack and stack[-1] == close_to_open[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+
+        return True if not stack else False
